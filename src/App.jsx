f34abb04be43e5d85f0e2fb6eaff43c31c07cd3b
ShivangSharma <99646444+sharma-shivang@users.noreply.github.com>
2024-03-02 11:17:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-red-500'>
+      <h1>kia hal hai bhaai ke</h1>
+    </div>
+  )
+}
+
+export default App
